21cf14d364dead8cc0c96f40474bc0ce8d7b2bd8
optimistic-Pig <miles1017@gmail.com>
2025-10-08 16:16:52 -0400

    Added sprites from tile map, added comments to code

--- Assets/Scripts/Mouse Manager.cs
+++ Assets/Scripts/Mouse Manager.cs
@@ -19,20 +19,31 @@ public class MouseManager : MonoBehaviour
     }
     void Update()
     {
+        // Input, is the way Unity manages player input, this works for controllers and keyboard//
+        //Because these are if condidtions and NOT if else, that means you can do multiple inputs at once
+
+        //Checks if the player is pressing D
         if(Input.GetKey(KeyCode.D))
         {
+            //Makes sure the direction of the force applied to the rigid body is to the right
             direction = Vector2.right;
+            //Using that right direction, multiplies force in taht direction, then multiplies gradually with impulse
             rb.AddForce(direction.normalized * moveForce, ForceMode2D.Impulse);
+            //Checks if the mouse if facing right, if not then flip the mouse
             if(isFacingRight == false)
             {
+                //sr is Sprite Renderer which we pull into the game scene from the mouse game object, we can do this because of the "public" advative
                 sr.flipX = true;
+                //Makes it so next time it will not flip it if the player is already moving right
                 isFacingRight = true;
             }
         }
         if(Input.GetKey(KeyCode.A))
         {
+            //Make direction left
             direction = Vector2.left;
             rb.AddForce(direction.normalized * moveForce, ForceMode2D.Impulse);
+            //If facing right, make the mlouse face left
             if(isFacingRight == true)
             {
                 sr.flipX = false;
@@ -41,7 +52,7 @@ public class MouseManager : MonoBehaviour
         }
         if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)) && isGrounded == true)
         {
-Debug.Log("In Up");
+            //Since force is being applied directionally, just make the direcrtion up to make them jumo
             direction = Vector2.up;
             rb.AddForce(direction.normalized * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
