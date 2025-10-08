f63b1c340dd668b394dc35c4f715fc5eb667b252
optimistic-Pig <miles1017@gmail.com>
2025-10-08 15:58:02 -0400

    Added mouse sprite, and changed circle collider to box

--- Assets/Scripts/Mouse Manager.cs
+++ Assets/Scripts/Mouse Manager.cs
@@ -1,27 +1,47 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MouseManager : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public SpriteRenderer sr;
     public float moveForce;
     public float jumpForce;
     private bool isGrounded = true;
+    private bool isFacingRight = true;
     private Vector2 direction;
 
     // Update is called once per frame
+    private string[] scenes;
+    private int currentScene = 0;
+    void Start()
+    {
+        scenes = new string[] {"Start", "Energy"};
+    }
     void Update()
     {
-    Debug.Log(isGrounded);
-        if(Input.GetKey(KeyCode.D)){
+        if(Input.GetKey(KeyCode.D))
+        {
             direction = Vector2.right;
             rb.AddForce(direction.normalized * moveForce, ForceMode2D.Impulse);
+            if(isFacingRight == false)
+            {
+                sr.flipX = true;
+                isFacingRight = true;
+            }
         }
-        else if(Input.GetKey(KeyCode.A)){
+        if(Input.GetKey(KeyCode.A))
+        {
             direction = Vector2.left;
             rb.AddForce(direction.normalized * moveForce, ForceMode2D.Impulse);
+            if(isFacingRight == true)
+            {
+                sr.flipX = false;
+                isFacingRight = false;
+            }
         }
-        else if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)) && isGrounded == true){
-        Debug.Log("In Up");
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)) && isGrounded == true)
+        {
+Debug.Log("In Up");
             direction = Vector2.up;
             rb.AddForce(direction.normalized * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
@@ -32,6 +52,12 @@ public class MouseManager : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground")){
             isGrounded = true;
+        }
+        if (collision.gameObject.CompareTag("Zone"))
+        {
+Debug.Log("in zone");
+            currentScene++;
+            SceneManager.LoadScene(scenes[currentScene]);
         }
     }
 
