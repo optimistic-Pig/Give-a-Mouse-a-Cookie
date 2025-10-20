86f845f23176e64f760923c8b46e921ba1be12f2
shannonpeelor <shannonpeelor@gmail.com>
2025-10-20 14:53:24 -0400

    Cat Arm Animation Added

---
+++ Assets/Scripts/CatArmMover.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CatArmMover
+{
+    //Link to what taught me how to rotate an object in Unity https://gamedevbeginner.com/how-to-rotate-in-unity-complete-beginners-guide/
+    void rotationPos()
+    {
+        Vector2 rotationPerFramePos = new Vector2(0, 2);
+    }
+    void rotationNeg()
+    {
+        Vector2 rotPerFrameNeg = new Vector2(0, -1);
+    }
+    void rotationCheck()
+    {
+    
+    }
+    void update()
+    {
+        rotationPos();
+    }
+}
