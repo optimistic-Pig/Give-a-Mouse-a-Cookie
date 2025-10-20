08658e5ab21b4fd5f902a8d8a6d50e664f865a23
shannonpeelor <shannonpeelor@gmail.com>
2025-10-20 15:44:01 -0400

    Ground Modification + Script started

--- Assets/Scripts/CatArmMover.cs
+++ Assets/Scripts/CatArmMover.cs
@@ -1,22 +1,32 @@
+using System;
 using UnityEngine;
 
-public class CatArmMover
+public class CatArmMover : MonoBehaviour 
 {
     //Link to what taught me how to rotate an object in Unity https://gamedevbeginner.com/how-to-rotate-in-unity-complete-beginners-guide/
     void rotationPos()
     {
-        Vector2 rotationPerFramePos = new Vector2(0, 2);
+        Vector2 rotPerFramePos = new Vector2(0, 2);
+        transform.Rotate(rotPerFramePos);
     }
     void rotationNeg()
     {
         Vector2 rotPerFrameNeg = new Vector2(0, -1);
+        transform.Rotate(rotPerFrameNeg);
     }
-    void rotationCheck()
+
+    void Update()
     {
-    
-    }
-    void update()
-    {
-        rotationPos();
+        for (int gameRunning = 0; gameRunning <999; gameRunning++)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                rotationPos();
+            }
+            for (int i = 0; i < 30; i++) 
+            { 
+                rotationNeg();
+            }
+        }
     }
 }
