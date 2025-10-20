55c6905f285d9320310ae089e603cad3201f17b5
shannonpeelor <shannonpeelor@gmail.com>
2025-10-20 16:11:15 -0400

    Arm Mover Update to Movement

--- Assets/Scripts/CatArmMover.cs
+++ Assets/Scripts/CatArmMover.cs
@@ -6,27 +6,16 @@ public class CatArmMover : MonoBehaviour
     //Link to what taught me how to rotate an object in Unity https://gamedevbeginner.com/how-to-rotate-in-unity-complete-beginners-guide/
     void rotationPos()
     {
-        Vector2 rotPerFramePos = new Vector2(0, 2);
-        transform.Rotate(rotPerFramePos);
+        int degreesPerSecond = 30;
+        transform.Rotate(new Vector3(0,0, degreesPerSecond) * Time.deltaTime);
     }
     void rotationNeg()
     {
-        Vector2 rotPerFrameNeg = new Vector2(0, -1);
-        transform.Rotate(rotPerFrameNeg);
+        int degreesPerSecond = -45;
+        transform.Rotate(new Vector3(0,0, degreesPerSecond) * Time.deltaTime);
     }
-
     void Update()
     {
-        for (int gameRunning = 0; gameRunning <999; gameRunning++)
-        {
-            for (int i = 0; i < 30; i++)
-            {
-                rotationPos();
-            }
-            for (int i = 0; i < 30; i++) 
-            { 
-                rotationNeg();
-            }
-        }
+        rotationNeg();
     }
 }
